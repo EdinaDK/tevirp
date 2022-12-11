87bb9abc12d39f905159f83668484b528067aac4
dasha <edinad@mer.ci.nsu.ru>
2022-12-11 17:03:30 +0700

    перевертыш

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Component} from "./components/Component";
 
 function App() {
-  return <Component text={"Привет!!!♣"}></Component>
+  return <Component text={"Привет!!!♣ здесь очень длинный текст —♥♥♥ текст текст"}></Component>
 }
 
 export default App;
---
+++ src/components/Component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useState} from 'react'
+import styles from './Component.module.css';
+
+type ComponentProps = {
+    text: string
+}
+
+export function Component({text}: ComponentProps) {
+    const [t, setT] = useState(text);
+
+    return <div className={styles.body}>
+        <div className={styles.text}>{t} <button className={styles.button} onClick={() => setT(t.split('').reverse().join(''))}>Reverse</button></div>
+
+    </div>;
+}
